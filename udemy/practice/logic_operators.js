055e57bba31ce2700c519e8fafa6a0f48f032b4d
Svetlana-Naumova <tiggernunki@mail.ru>
2023-07-15 14:40:47 +0300

    logic if

---
+++ udemy/practice/logic_operators.js
@@ -0,0 +1,49 @@
+/*const myNumber = parseInt(prompt("please enter the number", ""))
+if(myNumber < 18){
+    console.log("you are too young")
+}
+else if(myNumber >= 18){
+    console.log("correct age")
+}
+
+const myNumber = parseInt(prompt("please enter your number", ""))
+
+switch(myNumber){
+    case 17:
+        alert("too young!");
+        break;
+    case 16:
+        alert("too young!");
+        break;
+    case 18:
+        alert("correct number!");
+        break
+
+}*/
+
+
+//const guessTheNumber = parseInt(prompt("please enter the number"))
+
+//switch(guessTheNumber){
+//    case 19:
+//        alert("not the correct number");
+//        break;
+//    case 20:
+//        alert("correct number");
+//        break
+//}
+
+
+
+const hamburger = true;
+const fries = true;
+
+if (hamburger && fries){
+    console.log("im full")
+}
+
+
+
+
+
+
