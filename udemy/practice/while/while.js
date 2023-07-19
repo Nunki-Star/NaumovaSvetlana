66aba0376262b97b18d483cc2f7a38326e8334c6
Svetlana-Naumova <tiggernunki@mail.ru>
2023-07-19 22:58:15 +0300

    while practice

---
+++ udemy/practice/while/while.js
@@ -0,0 +1,67 @@
+/*let num = 50;
+while(num<55){
+    console.log(num);
+    num++
+}
+
+let myNum = 60;
+do{
+    console.log(myNum);
+    myNum++;
+}while(myNum<70)
+
+
+for(let i = 0; i < 15; i++){
+    if(i === 7){
+        //break;
+        continue;
+    }
+    console.log(i)
+}
+
+let num = 50;
+while(num<55){
+    console.log(num);
+    num++
+}
+
+let numnum = 95
+do{
+    console.log(numnum);
+    numnum++;
+}while(numnum<100);
+
+
+for(let i = 14; i < 20; i++){
+    if(i === 15){
+        continue;
+    }
+    console.log(i)
+}
+
+for(let i = 20; i>=10; i--){
+    if (i === 13){
+        break;
+    }
+    console.log(i)
+}
+
+for(let i = 0; i<=10; i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}*/
+
+let i = 2;
+while(i <= 16){
+    
+    if(i%2==0){
+        console.log(i);
+        i++;
+        
+        
+        
+    }
+    
+}
+
