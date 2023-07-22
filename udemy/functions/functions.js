ad7129e9265a65b585ba9abfc120446abfa7a76b
Svetlana-Naumova <tiggernunki@mail.ru>
2023-07-22 16:42:45 +0300

    function practice added

--- udemy/functions/functions.js
+++ udemy/functions/functions.js
@@ -1,4 +1,15 @@
-function sayHello(name){
+/*function sayHello(name){
     console.log("Hello, " + name)
 }
-sayHello("Svetlana")
+sayHello("Svetlana")*/
+
+
+//function sayHello(){
+   // alert("say hello!");
+//}
+//sayHello();
+
+function getTheDistance(speed, time){
+    alert("the distance is " + speed * time);
+}
+getTheDistance(6, 7)
