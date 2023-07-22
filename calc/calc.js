d997d0fcde9c2fe4016f170e8fb4257b50f24b72
Svetlana-Naumova <tiggernunki@mail.ru>
2023-07-22 19:04:02 +0300

    finctions added to calcilator

--- calc/calc.js
+++ calc/calc.js
@@ -1,1 +1,40 @@
+const x = null; 
+const y = null;
+const sign = ""
+
+function getNumber(num){
+    
+        if(x === null){
+            x=num
+        }
+        else{
+            y=num
+        }
+    }
+
+    function getSign(operation){
+        sign = operation
+    }
+
+    function result(){
+        var EL = document.getElementById('result');
+
+        switch(operVariable){
+            case "+": 
+                EL.innerText = x+y;
+                break;
+            case "-": 
+                EL.innerText = (x-y).toString();
+                break;
+            case "*": 
+                EL.innerText = (x*y).toString();
+                break
+            case "/":
+                EL.innerText = (x/y).toString();
+                break
+    
+        }
+    }
+
+
 
