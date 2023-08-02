99c227e6fa8f1557cf384ffa631ba3680e049543
Svetlana-Naumova <tiggernunki@mail.ru>
2023-08-02 10:35:39 +0300

    functions practice

--- udemy/functions_closure/function_closure.js
+++ udemy/functions_closure/function_closure.js
@@ -8,10 +8,60 @@ let myRoomArea = calculateRectangleArea(10, 10);
 console.log(`${myRoomArea} square meters`
 )*/
 
-function youSayGoodbye(){
+/*function youSayGoodbye(){
     alert("good bye!");
     function andIsayHello(){
         alert("hello!");
     }
     return andIsayHello;
 }
+
+let something = youSayGoodbye();
+something()*/
+
+/*function init(){
+    var name = "mozilla";
+    function displayName(){
+        alert (name);
+    }
+    displayName();
+}
+init();*/
+
+/*function makeFunc(){
+    var name = "mozilla";
+
+    function displayName(){
+        alert(name)
+    }
+    return displayName;
+}
+var myFunc = makeFunc();
+myFunc();*/
+
+/*let name = "John";
+
+function sayHi() {
+  alert("Hi, " + name);
+}
+
+name = "Pete";
+
+sayHi();*/
+
+function makeWorker() {
+    let name = "Pete";
+  
+    return function() {
+      alert(name);
+    };
+  }
+  
+  let name = "John";
+  
+  // create a function
+  let work = makeWorker();
+  
+  // call it
+  work();
+
