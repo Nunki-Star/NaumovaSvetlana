4b992a2792f7de2affa70ac41d4573f0ce490d9c
Svetlana-Naumova <tiggernunki@mail.ru>
2023-08-06 12:09:55 +0300

    array practice added

--- udemy/array/array.js
+++ udemy/array/array.js
@@ -1,4 +1,4 @@
-let groceries = ["milk", "water", "banana"]
+/*let groceries = ["milk", "water", "banana"]
 let addingItem = groceries.push("bread")
 console.log(addingItem)
 console.log(groceries)
@@ -10,3 +10,29 @@ let removeBack = groceries.pop()
 console.log(groceries)
 let removeFront = groceries.shift();
 console.log(groceries)
+
+let groceries2 = ["TEA", "COFEE"]
+let twoArrays = groceries.concat(groceries2)
+console.log(twoArrays)*/
+
+/*let names = ["merge", "homer", "bart"];
+function capitalizeitem(item){
+    let firstLetter = item.charAt(0).toUpperCase();
+    return firstLetter + item.slice(1);
+}
+let newArray = names.map(capitalizeitem)
+console.log(newArray)*/
+const myArray= [ 5, 7, 
+    [ 4, [2], 8, [1,3], 2 ], 
+    [ 9, [] ], 
+    1, 8
+  ]
+  ;
+function sumOfArray(arr){
+    let mySum = 0;
+    for(let i = 0; i<arr.length; i++){
+        mySum +=arr[i]
+    }
+    console.log(mySum);
+}
+sumOfArray(myArray)
