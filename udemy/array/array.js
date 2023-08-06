d1b0aad46635297ad5ad6ae484cd54bf5d8fac61
Svetlana-Naumova <tiggernunki@mail.ru>
2023-08-06 10:02:44 +0300

    add and remove items added

--- udemy/array/array.js
+++ udemy/array/array.js
@@ -1,3 +1,12 @@
-let groceries = ["milk", "banana", "bread"]
-groceries.push["water"]
+let groceries = ["milk", "water", "banana"]
+let addingItem = groceries.push("bread")
+console.log(addingItem)
+console.log(groceries)
+let addingItemFront = groceries.unshift("vitamins")
+console.log(addingItemFront)
+console.log(groceries)
+
+let removeBack = groceries.pop()
+console.log(groceries)
+let removeFront = groceries.shift();
 console.log(groceries)
