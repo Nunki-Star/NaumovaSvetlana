8bee5012f1c80f7344f5dbce0f90b95b67a32277
Svetlana-Naumova <tiggernunki@mail.ru>
2023-08-10 20:51:31 +0300

    A2+

--- codewars/disemovaltrolls/trols.js
+++ codewars/disemovaltrolls/trols.js
@@ -1,6 +1,14 @@
-function disemvowel(str) {
+/*function disemvowel(str) {
   
     var outputString=str.replace(/[aeiouAEIOU]/g,"");
     return outputString;
      }
-console.log(disemvowel("svetlana"))
+console.log(disemvowel("svetlana"))*/
+
+let str = " svetlana ";
+if(str.lastIndexOf(" ") !== -1 || str.indexOf(" ") === 0){
+    str.replace([" "], "");
+    console.log(str.indexOf("s"))
+}else{
+    console.log("no")
+}
