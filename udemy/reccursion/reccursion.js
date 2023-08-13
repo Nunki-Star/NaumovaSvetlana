909e1e79af11328fc12cc0d26f2f2bdc01380555
Sviatlana Navumava <tiggernunki@mail.ru>
2023-08-13 18:08:15 +0300

    reccursion home task

--- udemy/reccursion/reccursion.js
+++ udemy/reccursion/reccursion.js
@@ -18,15 +18,81 @@ const answer = factorial(6)
 console.log(answer)*/
  
 /*function sumOfArrays(arr){
-    add = 0;
+   let add = 0;
     for(let i = 0; i<arr.length; i++){
-       
-        add += arr[i];
+       for(let k = 0; k<arr[i].length; k++){
+        add += arr[i][k];
+       }
+        
     } 
 
     return add;
 }
-console.log(sumOfArrays([10, 3, 4]))*/
+console.log(sumOfArrays([10, 3, 4[2, 5]]))*/
 
+/*function getArraysSumm(arr) {
+  
 
-const a = [2, 3, [4, 6]];
+    let sum = 0;
+    
+  
+    for (let i = 0; i < arr.length; i++) {
+      
+  
+      for (let j = 0; j < arr[i].length; j++) {
+        
+      
+        sum += arr[i][j];
+        
+      }
+      
+    }
+    
+    return sum;
+    
+  }
+  
+  console.log(getArraysSumm([ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8]));*/
+
+/*function sumOfMultipleArrays(arr){/
+    totalCount = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i]) == false){
+             totalCount+=arr[i];
+            }else if( Array.isArray(arr[i] == true)){
+                return sumOfMultipleArrays(arr[i])
+            }
+        }
+    }
+            
+
+console.log(sumOfMultipleArrays([2, 3, 4]))*/
+
+/*function sumOfMultipleArrrays(arr){
+    totalCount = 0;
+    for(let i = 0; i < arr.length; i++){
+        totalCount += arr[i];
+        
+    }if(Array.isArray(arr[i]) === true){
+        return sumOfMultipleArrrays();
+    }
+}
+console.log(sumOfMultipleArrrays([2, 3[2, 3]]))*/
+
+function summOfArrays(arr) {
+	let totalSumm = 0;
+	
+	for (let i = 0; i<arr.length; i++) {
+		if (typeof i == 'object') {
+			totalSumm += summOfArrays(i);
+		} else {
+			sum += i;
+		}
+	}
+	
+	return totalSumm;
+}
+
+console.log(summOfArrays([ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8]));
+  //[ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8]
+
