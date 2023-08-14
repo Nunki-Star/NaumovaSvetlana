f28791b9dd56212ec4facfd2e5b45fdc15fe224d
Sviatlana Navumava <tiggernunki@mail.ru>
2023-08-14 20:11:01 +0300

    codewars remove  replace method added

---
+++ codewars/removestartend/remove.js
@@ -0,0 +1,11 @@
+function array(string) {
+    if(string.length === 0 && str.length === 1 && str.length === 2){
+      return null;
+    }else {
+      let stringPop = string.pop();
+      let stringShift = string.shift();
+      let replaced = sliceTheStRing.replace((/[\s.,%]/g, ''));
+      return stringShift;
+    
+  }
+}
