ee3760a6a21406d1a0af6efb7136da7cbd25f7f4
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-15 16:21:43 +0300

    a2 task corrected

--- A2+/A2.js
+++ A2+/A2.js
@@ -1,7 +1,8 @@
 function removeSpaceWithNoTrim(str){
     //for(let i = 0; i<str.length; i++){
         if(str.indexOf(" ") === 0 && str.lastIndexOf(" ") !== -1){
-            let outputString=str.replace(/ /g,"");
+            //let outputString=str.replace(/ /g,"");
+            let outputString = str.replace(" ","");
             return outputString;
         }
     }
