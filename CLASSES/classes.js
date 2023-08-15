608be6d0a04ca3e6fdc9456fc27c3eecdac4a835
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-15 22:32:16 +0300

    classes practice

---
+++ CLASSES/classes.js
@@ -0,0 +1,21 @@
+/*class Person{
+    name = "Any";
+    hair = this.hair;
+    age = this.age;
+    greeting(){
+        console.log("hello," + this.name + "." + "She is "+ this.age + "." + "her hair is " + this.hair)
+        
+    }
+}
+
+let firstPerson = new Person();
+firstPerson.name = "Svetlana";
+firstPerson.age = 36;
+firstPerson.hair = "straight"
+let secondPerson = new Person();
+secondPerson.name = "Galina";
+secondPerson.age = 66;
+secondPerson.hair = "curly"
+firstPerson.greeting()
+secondPerson.greeting()*/
+
