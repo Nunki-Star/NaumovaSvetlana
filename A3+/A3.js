9bf54e640a62621a29d37141d2ab02f8a600e075
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-16 16:35:21 +0300

    a4 title was replaced with a3

--- A3+/A3.js
+++ A3+/A3.js
@@ -1,12 +1,48 @@
 /*function isPalindrom(str){
+    let loweCaseLetters = str.toLowerCase();
+    let splitString = loweCaseLetters.split("");
+    let reversedArr = splitString.reverse();
     for(let i = 0; i < str.length; i++){
-      return  str.replace("ё", "ggg");
+        for(let k = 0; k<reversedArr.length; k++){
+            if(str[i] === reversedArr[k]){
+                let truePalindrom = "the string is palindrom";
+                return truePalindrom
+            }else{
+                let falsePalindrom = "the string is not a palindrom"
+                return falsePalindrom
+            }
+        }
+    }
+
+   
+}
+console.log(isPalindrom("anna"));
+console.log(isPalindrom("svetlana"));
+console.log(isPalindrom("Искать такси"))*/
+
+
+function isPalindrom(str){
+    let lettersToLowerCase = str.toLowerCase();
+    let replaceSpace = lettersToLowerCase.replace(" ", "");
+    let replacedPunctuationMarks = replaceSpace.replace(/[\.,%!?]/g, '');
+    let revercedStr = ""
+    for(let i = replacedPunctuationMarks.length - 1; i>=0; i--){
+        revercedStr += replacedPunctuationMarks[i];
+    }
+    for( let i = 0; i < replacedPunctuationMarks.length; i++){
+        for (let k = 0; k < revercedStr.length; k++){
+            if(replacedPunctuationMarks[i] === revercedStr[k]){
+                return `Your string "${str}" is palindrom`
+            }else{
+                return `Your string "${str}" is not a palindrom`
+            }
+        }
     }
 }
 
-console.log(isPalindrom("ёмаё"))*/
+console.log(isPalindrom("Anna"));
+console.log(isPalindrom("Искать!!!!! такси????"));
 
-
-
-
+        
     
+
