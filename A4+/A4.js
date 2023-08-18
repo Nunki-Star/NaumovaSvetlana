925bcf9ae7926d01f83d8da5803d90f584bde4db
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-18 19:04:43 +0300

    a4 updates added

--- A4+/A4.js
+++ A4+/A4.js
@@ -1,3 +1,7 @@
 function recursPalindrom(str){
+    let toSmallLettes = str.toLowerCase();
+    let removeSpace = toSmallLettes.replace(" ", "");
+    return removeSpace
     
 }
+console.log(recursPalindrom("Svetala Svetlana"))
