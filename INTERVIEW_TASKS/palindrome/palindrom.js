50bdc1b1222476d23979c563ebed32ce3b84c59d
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-19 18:13:52 +0300

    palindrom task added to interview file

---
+++ INTERVIEW_TASKS/palindrome/palindrom.js
@@ -0,0 +1,26 @@
+function isPalindrome(str){
+    let strLower = str.toLowerCase();
+    let removeSpace = strLower.replaceAll(" ", "");
+    let removeMarks = removeSpace.replace(/[\.,;!?]/g, "");
+    let revesedStr = "";
+    for(let i = removeMarks.length - 1; i >= 0; i--){
+        revesedStr += removeMarks[i];
+    }
+    
+    for(let k = 0; k<removeMarks.length; k++){
+        for(let j = 0; j<revesedStr.length; j++){
+            if(removeMarks[k] === revesedStr[j]){
+                return `${str} is palindrome`
+            }else{
+                return `${str} is not a palindrome`
+            }
+        }
+    }
+}
+
+
+console.log(isPalindrome("     SVETLANA..?;      svetlana"));
+console.log(isPalindrome("АнН               а"));
+
+console.log(isPalindrome("a!!!!!       NNA"));
+console.log(isPalindrome("SVRTKHFKJ     !!!!!!??????AABB"))
