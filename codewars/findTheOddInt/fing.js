ef357f76d0c4da72f2be29c1830453c145a7ca01
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-21 11:12:06 +0300

    codewars task eddited

--- codewars/findTheOddInt/fing.js
+++ codewars/findTheOddInt/fing.js
@@ -1,4 +1,16 @@
 function findOdd(A) {
-    //happy coding!
-    return 0;
+   let sortedArr = A.sort();
+   let checkingArr = []
+    if(sortedArr === 0 || sortedArr.length === 1){
+        return sortedArr[0]
+    }else if(sortedArr[0] !== sortedArr[1]){
+        return sortedArr[0]
+    }else if(sortedArr[0] === sortedArr[1]){
+        r
+    }
+    
   }
+
+console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1]))
+console.log(findOdd([5]))
+
