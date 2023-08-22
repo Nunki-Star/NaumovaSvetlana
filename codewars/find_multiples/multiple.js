a24d9183dfd64b998b3d7ff3b3b5d4381060b64a
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-22 03:54:58 +0300

    codewars task completed

--- codewars/find_multiples/multiple.js
+++ codewars/find_multiples/multiple.js
@@ -1,4 +1,15 @@
 function findMultiples(integer, limit) {
-    let result = [];
-    while tru
+    let arr = [];
+    sum = 0
+    while(true){
+      sum += integer;
+
+      arr.push(sum);
+      if(sum > limit){
+        return arr.slice(0, -1);
+    }
+    
+    }
+    
   }
+console.log(findMultiples(4, 27))
