60192e8963561a6bfa748ce5754e2e83af2c3dce
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-23 19:54:03 +0300

    chocolate task added

---
+++ codewars/chocolate_divider/cocolate.js
@@ -0,0 +1,11 @@
+function breakChocolate(n,m) {
+    let row = 0 
+   if(m === 1){
+    row = n-1
+    }else if(m === 0 || n === 0){
+        return 0
+    }
+
+}
+
+console.log(breakChocolate(0, 0))
