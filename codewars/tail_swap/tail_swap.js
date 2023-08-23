4311e79c35811f5a270d3c59f91b7b24a0539941
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-23 17:15:24 +0300

    tail swap task completed

--- codewars/tail_swap/tail_swap.js
+++ codewars/tail_swap/tail_swap.js
@@ -1,4 +1,4 @@
-function tailSwap(arr) {
+/*function tailSwap(arr) {
    let item1 = arr[0]
    let item2 = arr[1];
    let itemNumbers1 = [];
@@ -49,4 +49,60 @@ function tailSwap(arr) {
     return finalArr
 
   }
-console.log(tailSwap(["a:12345", "777:xyz"]))
+console.log(tailSwap(["a:12345", "777:xyz"]))*/
+
+
+
+function tailSwap(arr){
+    let beforeDotts1 = [];
+    let afterDotts1 = [];
+    let beforeDotts2 = [];
+    let afterDotts2 = [];
+    let first = arr[0];
+    let second = arr[1]
+    let firstArr = first.split("");
+    let secondArr = second.split("");
+    let finalArr1 = [];
+    let finalArr2 = [];
+    for(let i = 0; i<firstArr.length; i++){
+        beforeDotts1.push(firstArr[i]);
+        if(firstArr[i] === ':'){
+            break
+        }
+    }
+    for(let j = firstArr.length - 1; j>= 0; j--){
+        afterDotts2.unshift(firstArr[j]);
+        if(firstArr[j] === ":"){
+            break
+        }
+        
+    }
+
+    for(let h = 0; h<secondArr.length; h++){
+        beforeDotts2.push(secondArr[h]);
+        if(secondArr[h] === ":"){
+            break
+        }
+    }
+
+    for(let k = secondArr.length -1; k>= 0; k--){
+        afterDotts1.unshift(secondArr[k]);
+        if(secondArr[k] === ":"){
+            break
+        }
+
+    }
+    afterDotts1.shift();
+    afterDotts2.shift();
+  
+   a =  beforeDotts1.concat(afterDotts1);
+   b = beforeDotts2.concat(afterDotts2);
+   a1 = a.join("");
+   b1 = b.join("");
+   finalArr1.push(a1);
+   finalArr2.push(b1);
+   total = finalArr1.concat(finalArr2)
+    return total
+    
+}
+console.log(tailSwap(['abc:123', 'cde:456']))
