13cb8ce35d20afd4eaade3bd278d0e3b7676caef
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-24 21:31:36 +0300

    array task from codewars completed

--- codewars/array_with_coma/array.js
+++ codewars/array_with_coma/array.js
@@ -1,9 +1,11 @@
-/*function printArray(array){
-    let str = "";
+function printArray(array){
+
+    a= array.join()
+    /*let str = "";
     for(let i = 0; i<array.length; i++){
-        return array[i]
-    }
-    return str
+       str+=array[i]
+    }*/
+    return a
   }
-  console.log(printArray(["h","o","l","a"]))*/
+  console.log(printArray(["h","o","l","a"]))
   console.log("hi")
