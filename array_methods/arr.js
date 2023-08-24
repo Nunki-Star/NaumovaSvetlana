80ea5a7e5c6c06f7e26cccb90a3a9a1563a8da7b
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-24 21:18:38 +0300

    arr practice completed

--- array_methods/arr.js
+++ array_methods/arr.js
@@ -43,12 +43,17 @@ a.forEach(myFunc)*/
 
 
 function accum(s){
-    s = s.split("");
-    s.forEach(function(i){
-    
-        console.log(i + "-")
-    })
-    return s
+    const result = []
+  const lowerStr = s.toLowerCase()
+
+  for(let i = 0; i < lowerStr.length; i++) {
+    let str = lowerStr[i].toUpperCase()
+    for(let j = 0; j < i; j++) {
+      str += lowerStr[i]
+    }
+    result.push(str)
+}
+    return result
 }   
 
 console.log(accum("abcd"))
