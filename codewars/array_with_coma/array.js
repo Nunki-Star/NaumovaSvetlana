ec4785cd0f1c0d05d8ea6dccc42a31f4c075ff24
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-24 21:25:37 +0300

    array folders for codewrs completed

---
+++ codewars/array_with_coma/array.js
@@ -0,0 +1,9 @@
+/*function printArray(array){
+    let str = "";
+    for(let i = 0; i<array.length; i++){
+        return array[i]
+    }
+    return str
+  }
+  console.log(printArray(["h","o","l","a"]))*/
+  console.log("hi")
