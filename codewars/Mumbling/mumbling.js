2aec9103156289b5b9ce50d3b92ba49437cb1245
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-24 13:32:08 +0300

    codewats mumbling

--- codewars/Mumbling/mumbling.js
+++ codewars/Mumbling/mumbling.js
@@ -1,15 +1,10 @@
 function mumbling(str){
-    let arr = [];
-    for(let i = 0; i<str.length; i++){
-        arr += str[i];
-       
+    arr = [];
+    for(let i = 0; i < str.length; i++){
+        str[i].split(" ")
     }
-    /*arr = arr.slice(0, -1);*/
-    for(let k = 0; k<arr.length; k++) {
-        
-    }  
     return arr
-   }
-   
-
-console.log(mumbling("123"))
+    
+    
+} 
+console.log(mumbling("ZpglnRxqenU"))
