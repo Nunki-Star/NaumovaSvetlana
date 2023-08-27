c0ce44081bd8bc5b13d14c24394b3f41e910a91d
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-27 21:05:22 +0300

    maket parctise

--- interner_store/store.js
+++ interner_store/store.js
@@ -1,1 +1,40 @@
+const cosmetics = [
+{"id":"card1",
+"url":"",
+"title":"lapraire"
+},
+{"id":"",
+"url":"",
+"title":""
+},
+{"id":"",
+"url":"",
+"title":""
+},
+{"id":"",
+"url":"",
+"title":""
+},
+{"id":"",
+"url":"",
+"title":""
+}
+
+]
+const app = document.getElementById("app");
+let htmlTags = "";
+cosmetics.forEach(user =>{
+    htmlTags+=`
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>`
+})
+
+app.innerHTML = htmlTags;
+
 
