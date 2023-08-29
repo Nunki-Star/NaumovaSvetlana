008ea181ac71ccc9843e23c36eccc6f0dec19686
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-08-29 13:26:28 +0300

    dom navigation practice

---
+++ MAKET/maket.js
@@ -0,0 +1,19 @@
+const documentBody = document.body;
+console.log(documentBody);
+const docFirstChild = document.firstChild;
+console.log(docFirstChild);
+const docLastChild = document.lastChild
+console.log(docLastChild)
+
+const kidds = documentBody.children;
+console.log(kidds);
+
+const kifsFirst = docFirstChild.children;
+console.log(kifsFirst);
+
+
+const kidsLast = docLastChild.children;
+console.log(kidsLast);
+
+const kidsNode = docLastChild.childNodes;
+console.log(kidsNode)
