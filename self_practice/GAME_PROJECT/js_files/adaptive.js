c54e8c297fe48ff10d27811adde2db6562d8e277
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-10-24 22:25:21 +0300

    game added to index file

--- self_practice/GAME_PROJECT/js_files/adaptive.js
+++ self_practice/GAME_PROJECT/js_files/adaptive.js
@@ -1,13 +1,27 @@
 window.matchMedia("(orientation: portrait)").addEventListener("change", e=>{
     const portrait = e.matches;
-    
+    const mainColor = document.querySelector(".main")
     if(portrait){
-        const mainColor = document.querySelector(".main")
+        
+       
            mainColor.style.display = "none";
-        mainColor.style.backgroundColor = "black";
+           document.body.style.background="orangered";
+           document.body.style.display = "flex";
+           document.body.style.flexDirection = "column";
+           document.body.style.justifyContent = "center";
+           document.body.style.alignItems = "center"
+           document.querySelector(".phone").style.display = "block";
+           document.querySelector(".phone").style.marginTop="50%"
+           document.querySelector(".message").style.display = "block",
+           document.querySelector(".message").style.fontFamily = "Jelantik";
+           document.querySelector(".message").style.fontSize="30px"
 
 
     }else{
-        
+        /*;
+        document.body.style.backgroundImage="url(../images/gradient-forest-landscape.jpg.avif)";*/
+        document.querySelector(".phone").style.display = "none";
+        document.querySelector(".message").style.display = "none"
+        mainColor.style.display = "block"
     }
  })
