caadeab09d5c6f44a1cf8f13ebd64046e551d64b
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-11-02 20:56:19 +0300

    64 folders moved to self practice folder

--- DONE/B1+/b1.js
+++
@@ -1,8 +0,0 @@
-function getTheAge(year){
-    const century = Math.ceil(year/100);
-    return `${year} year belongs to ${century} century`
-}
-
-console.log(getTheAge(1980));
-console.log(getTheAge(2000));
-console.log(getTheAge(2001))
