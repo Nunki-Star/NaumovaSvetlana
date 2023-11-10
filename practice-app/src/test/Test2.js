244f99c5e5631ebed2218e11a28d46a2975759cf
SVIATLANA NAVUMAVA <tiggernunki@mail.ru>
2023-11-10 16:27:34 +0300

    react testing tasks import export

---
+++ practice-app/src/test/Test2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Test2(){
+    return(
+        <h2>Test2</h2>
+    )
+}
+export default Test2
---
+++ practice-app/src/test/Total.js
@@ -0,0 +1,13 @@
+import Test1 from './Test1';
+import Test2 from './Test2'
+
+function Total(){
+    return(
+        <>
+        <Test2/>
+        <Test1/>
+        </>
+    )
+}
+
+export default Total
