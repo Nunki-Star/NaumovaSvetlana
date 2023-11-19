4a56ba8e81e9330ded49e01caf43902477a31d1c
Svetlana <tiggernunki@mail.ru>
2023-11-19 23:16:21 +0300

    rainbow hoc completed

--- rainbow-frame-hoc/src/hoc/withRainbowFrame.jsx
+++ rainbow-frame-hoc/src/hoc/withRainbowFrame.jsx
@@ -4,9 +4,7 @@ import React from 'react';
 export const withRainbowFrame = (colors) => (Fragment) => ({ ...props }) => {
     const reduceCB = (accum, cur) => {
         return (
-            <div style={{ border: "solid 10px " + cur, padding: "10px" }}>
-                {accum}
-            </div>
+            <div style={{ border: "solid 10px " + cur, padding: "10px" }}>{accum}</div>
         )
     }
     const rainbowFrame = colors.reduce(reduceCB, <Fragment {...props}/>);
