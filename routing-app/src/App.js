7420df8d978bf6dbc27fe858be286f46229e788d
Svetlana <tiggernunki@mail.ru>
2023-11-23 19:35:46 +0300

    react-router-dom added

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import About from './About';
+import Users from './Users';
+import Main from './Main';
+
+import {BrowserRouter,Router,Routes,Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/users" element={<Users/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
