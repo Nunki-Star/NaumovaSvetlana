8ecefd56b98805b92898949a28c9a837e40dea3d
Svetlana <tiggernunki@mail.ru>
2023-12-16 15:38:55 +0300

    buttons taggs adjusted

--- react-project/src/components/HeroSection.js
+++ react-project/src/components/HeroSection.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src="../puplic/videos/video-2.mp4" autoPlay loop muted/>
-      <h1>Adventure awaits</h1>
+      <video loop="loop" autoplay="autoplay" muted="muted">
+        <source src='/videos/video-1.mp4' type="video/mp4"/>
+      </video>
+      <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className="hero-bths">
-        <Button className='btns' buttonStyle='btn--outline'
-        buttonSize='ntn--large'>
-            GET STARTED
+      <div className='hero-btns'>
+        <Button className='btns' buttonStyle='btn--outline'buttonSize='btn--large'>
+          GET STARTED
+        </Button>
+        <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large' onClick={console.log('hey')}>
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
