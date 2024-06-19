182cbc2734a723239f37759ec02ac893f531f67b
Svetlana <tiggernunki@mail.ru>
2024-06-19 22:14:47 +0400

    practise refreshing

---
+++ js/js.js
@@ -0,0 +1,30 @@
+let a = 2;
+let b = 3;
+console.log(a+b);
+
+
+
+let obj = {
+    name:"Svetlana",
+    age:37,
+    isMarried:false
+   
+}
+
+console.log(obj.name, obj.age, obj.isMarried);
+
+
+let arr = ["a", "b", 3, {}, [1, 2, 3]];
+console.log(arr[1]);
+console.log(arr[4][0])
+
+
+let obj2 = {
+    a:"123",
+    b:123
+}
+obj2.c = 222;
+
+console.log(obj2);
+
+console.log(obj2.c)
